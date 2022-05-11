26a0da785b4dfb555fe2a3eda472c355741d7166
infraket <aveacvale@mail.ru>
2022-05-11 17:20:45 +0400

    Update data.py

--- src/project_ml/data.py
+++ src/project_ml/data.py
@@ -5,10 +5,16 @@ from pandas_profiling import ProfileReport
 
 def get_dataset(csv_path: Path, eda:bool) -> Tuple[pd.DataFrame, pd.Series]:
     dataset = pd.read_csv(csv_path)
+    features = dataset.drop("Cover_Type", axis=1)
+    target = dataset["Cover_Type"]
     print(dataset.head())
+    # click.echo(f"Dataset shape: {dataset.shape}.")
+
+
 
     if eda:
         profile = ProfileReport(dataset, minimal=True)
         profile.to_file("output.html")
+    return  features, target
 
 
